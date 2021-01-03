df218a2e8df15cf2006c53d87a6dcb8e240b326f
Timothée Sterle <timothee-s@mailoo.org>
2021-01-03 20:09:03 +0100

    refactor(blob): iterate over chars lazily

--- lua/if_lua_compat/blob.lua
+++ lua/if_lua_compat/blob.lua
@@ -3,23 +3,28 @@
 
 local Blob
 
+--- @param str string
+--- @param index number
+--- @return number, string
+local function str_iter(str, index)
+    if index == #str then return end
+    index = index + 1
+    return index, str:sub(index, index)
+end
+
 --- @param bytes_str string|number
---- @return table<number, number>
-local function string_to_bytes(bytes_str)
+--- @return function, string, number
+local function str_to_bytes(bytes_str)
     if type(bytes_str) == 'number' then bytes_str = tostring(bytes_str) end
     if type(bytes_str) ~= 'string' then error('string expected, got ' .. type(bytes_str)) end
-    local bytes = {}
-    for char in vim.gsplit(bytes_str, '') do
-        table.insert(bytes, string.byte(char))
-    end
-    return bytes
+    return str_iter, bytes_str, 0
 end
 
 local blob_methods = {
     --- @param self Blob
     --- @param bytes_str string|number
     add = function(self, bytes_str)
-        for _, byte in ipairs(string_to_bytes(bytes_str)) do
+        for _, byte in str_to_bytes(bytes_str) do
             table.insert(self, byte)
         end
     end,
@@ -39,8 +44,10 @@ local blob_mt = {
 --- @return Blob
 function Blob(bytes_str)
     local blob = {}
-    for idx, byte in ipairs(string_to_bytes(bytes_str)) do
-        blob[idx - 1] = byte
+    if bytes_str ~= nil then
+        for idx, byte in str_to_bytes(bytes_str) do
+            blob[idx - 1] = byte
+        end
     end
     return setmetatable(blob, blob_mt)
 end
